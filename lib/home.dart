3691eb98096219a556d3a67cc0edf23b4384622c
0xricoard <mriko743@gmail.com>
2022-12-26 22:23:06 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+//import 'package:resepku_app/homeold.dart';
+import 'package:resepku_app/indo.dart';
+import 'package:resepku_app/japan.dart';
+import 'package:resepku_app/western.dart';
+import 'recipe_card.dart';
+//import 'indo.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+final List<String> judul = <String>[
+  'Makanan Indonesia',
+  'Makanan Western',
+  'Makanan Jepang'
+];
+final List<String> rate = <String>['4.5', '4.3', '4.5'];
+final List<String> waktu = <String>[
+  '10-15 Menit',
+  '10-25 Menit',
+  '10-20 Menit'
+];
+final List<String> gambar = <String>[
+  'assets/images/nasgor.jpg',
+  'assets/images/steak.jpg',
+  'assets/images/sushi.jpg'
+];
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.restaurant_menu),
+              SizedBox(width: 10),
+              Text('Resepku App'),
+            ],
+          ),
+        ),
+        //agar bisa discroll untuk perangkat layar kecil
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // menambahkan teks Daftar Resep
+              Center(
+                child: Text(
+                  'Daftar Resep',
+                  style: TextStyle(
+                    fontSize: 33,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              // bagian menu Makanan Indonesia
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: judul.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      if (index == 0) {
+                        //  navigasi ke halaman Makanan Indonesia
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Indo()));
+                      } else if (index == 1) {
+                        //  navigasi ke halaman Makanan Western
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Western()));
+                      } else if (index == 2) {
+                        // navigasi ke halaman Makanan Jepang
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Japan()));
+                      }
+                    },
+                    //mengambil data array dan style dari recipe_card
+                    child: RecipeCard(
+                        title: judul[index],
+                        cookTime: waktu[index],
+                        rating: rate[index],
+                        thumbnailUrl: gambar[index]),
+                  );
+                },
+              ),
+            ],
+          ),
+        ));
+  }
+}
